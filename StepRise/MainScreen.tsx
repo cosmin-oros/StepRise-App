63e493b130003db75e55ada6c87c4953bdbff7c0
cosmin-oros <oroscosmin09@gmail.com>
2023-03-20 20:02:21 +0200

    layout design main screen

--- StepRise/MainScreen.tsx
+++ StepRise/MainScreen.tsx
@@ -13,26 +13,39 @@ const MainScreen: React.FC<Props> = ({ steps, level, xp, challengesCompleted, wa
   return (
     <View style={styles.container}>
       <Text style={styles.title}>StepRise</Text>
+
       <View style={styles.card}>
         <Text style={styles.cardTitle}>Today's Steps</Text>
         <Text style={styles.cardValue}>{steps}</Text>
       </View>
+
       <View style={styles.statsContainer}>
-        <View style={styles.stat}>
-          <Text style={styles.statTitle}>Level</Text>
-          <Text style={styles.statValue}>{level}</Text>
+        <View style={styles.card}>
+          <View style={styles.stat}>
+            <Text style={styles.statTitle}>Level</Text>
+            <Text style={styles.statValue}>{level}</Text>
+          </View>
         </View>
-        <View style={styles.stat}>
-          <Text style={styles.statTitle}>XP</Text>
-          <Text style={styles.statValue}>{xp}</Text>
+
+        <View style={styles.card}>
+          <View style={styles.stat}>
+            <Text style={styles.statTitle}>XP</Text>
+            <Text style={styles.statValue}>{xp}</Text>
+          </View>
         </View>
-        <View style={styles.stat}>
-          <Text style={styles.statTitle}>Challenges Completed</Text>
-          <Text style={styles.statValue}>{challengesCompleted}</Text>
+
+        <View style={styles.card}>
+          <View style={styles.stat}>
+            <Text style={styles.statTitle}>Challenges Completed</Text>
+            <Text style={styles.statValue}>{challengesCompleted}</Text>
+          </View>
         </View>
-        <View style={styles.stat}>
-          <Text style={styles.statTitle}>Water Consumed</Text>
-          <Text style={styles.statValue}>{waterConsumed} mL</Text>
+
+        <View style={styles.card}>
+          <View style={styles.stat}>
+            <Text style={styles.statTitle}>Water Consumed</Text>
+            <Text style={styles.statValue}>{waterConsumed} mL</Text>
+          </View>
         </View>
       </View>
     </View>
@@ -42,7 +55,7 @@ const MainScreen: React.FC<Props> = ({ steps, level, xp, challengesCompleted, wa
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#082438',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -50,9 +63,12 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: 'bold',
     marginBottom: 32,
+    textShadowColor: '#ffffff',
+    textShadowRadius: 10.0,
+    color: '#ffffff',
   },
   card: {
-    backgroundColor: '#f0f0f0',
+    backgroundColor: '#2C2C2C',
     padding: 16,
     borderRadius: 8,
     marginBottom: 16,
@@ -63,15 +79,15 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 8,
+    color: '#ffffff',
   },
   cardValue: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#ffffff',
   },
   statsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
+    paddingHorizontal: 16
   },
   stat: {
     alignItems: 'center',
@@ -80,10 +96,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     marginBottom: 8,
+    color: '#ffffff',
   },
   statValue: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: '#ffffff',
   },
 });
 
